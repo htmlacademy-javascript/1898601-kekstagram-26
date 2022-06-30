758441acf91dda0fc2adc6a04d98b213fa2b4541
Anton Fomichev <avfomichev_2@edu.hse.ru>
2022-06-30 23:25:27 +0300

    functions modified: gitRandInRange, isShortString

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-function getRandInRange(begin, end){
+function getRandInRange(begin, end) {
     [begin, end] = [Math.min(begin, end), Math.max(begin, end)];
     return begin + Math.floor(Math.random() * (end - begin + 1));
 }
 
-function isShortString(string, maxLength){
+function isShortString(string, maxLength) {
     return string.length <= maxLength;
 }
+
+getRandInRange(1, 5);
+isShortString("Hello my name is...", 140);
