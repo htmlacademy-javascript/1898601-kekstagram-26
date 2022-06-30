441050ff7e2a42db2cb512cbd49233d488b7bea8
Anton Fomichev <avfomichev_2@edu.hse.ru>
2022-06-30 23:32:14 +0300

    file fixed: main.js. Indentations, quotes fixed.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function getRandInRange(begin, end) {
-    [begin, end] = [Math.min(begin, end), Math.max(begin, end)];
-    return begin + Math.floor(Math.random() * (end - begin + 1));
+  [begin, end] = [Math.min(begin, end), Math.max(begin, end)];
+  return begin + Math.floor(Math.random() * (end - begin + 1));
 }
 
 function isShortString(string, maxLength) {
-    return string.length <= maxLength;
+  return string.length <= maxLength;
 }
 
 getRandInRange(1, 5);
-isShortString("Hello my name is...", 140);
+isShortString('Hello my name is...', 140);
