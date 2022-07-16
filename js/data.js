9818fa7b3fea8d959c820695f315d1242be30ce1
Anton Fomichev <avfomichev_2@edu.hse.ru>
2022-07-16 15:46:51 +0300

    createRandomIdFromRangeGenerator function added.

--- js/data.js
+++ js/data.js
@@ -16,15 +16,13 @@ const COMMENT_NAMES = [
   'Антон', 'Иван', 'Мария', 'Кекс', 'Василий', 'Елена Ивановна',
 ];
 
-const PICTURE_DESCTIPTIONS = [
+const PICTURE_DESCRIPTIONS = [
   'Данную фотографию я очень люблю.',
   'Эта фотография вызывает у меня тёплые чувства',
   'Всем моим братьям салам',
   `Номера тачек, хата на девятом, знай кто платит (Rrrra)
   Street credibility и не надо платин, (Ski,ski)`
 ];
-
-const commentIds = [];
 
 const PHOTOS_SIZE = 25;
 const MAX_STRING_LENGTH = 140;
@@ -34,6 +32,8 @@ const MIN_COMMENTS = 0;
 const MAX_COMMENTS = 5;
 const MIN_AVATAR_VALUE = 1;
 const MAX_AVATAR_VALUE = 6;
+const MIN_COMMENT_ID = 0;
+const MAX_COMMENT_ID = 1000;
 
 
 const getPictureUrl = function (i) {
@@ -41,14 +41,23 @@ const getPictureUrl = function (i) {
 };
 
 
-const getCommentId = function () {
-  const randomId = getRandomPositiveInteger(0, 1000);
-  if (commentIds.includes(randomId)) {
-    getCommentId();
-  }
-  commentIds.push(randomId);
-  return randomId;
+const createRandomIdFromRangeGenerator = function (min, max) {
+  const previousValues = [];
+  return function(){
+    if(previousValues.length === max - min + 1){
+      return null;
+    }
+    let randomId = getRandomPositiveInteger(min, max);
+    while(previousValues.includes(randomId)){
+      randomId = getRandomPositiveInteger(min, max);
+    }
+    previousValues.push(randomId);
+    return randomId;
+  };
 };
+
+
+const generateCommentId = createRandomIdFromRangeGenerator(MIN_COMMENT_ID, MAX_COMMENT_ID);
 
 
 const getCommentAvatar = function (begin, end) {
@@ -64,7 +73,7 @@ const getCommentMessage = function () {
 
 const createComment = function () {
   const comment = {
-    id: getCommentId(),
+    id: generateCommentId(),
     avatar: getCommentAvatar(MIN_AVATAR_VALUE, MAX_AVATAR_VALUE),
     message: getCommentMessage(),
     name: getRandomFromArray(COMMENT_NAMES),
@@ -77,7 +86,7 @@ const createPhoto = function (element, index) {
   const photo = {
     id: index + 1,
     url: getPictureUrl(index + 1),
-    description: getRandomFromArray(PICTURE_DESCTIPTIONS),
+    description: getRandomFromArray(PICTURE_DESCRIPTIONS),
     likes: getRandomPositiveInteger(MIN_LIKES, MAX_LIKES),
     comments: Array.from({ length: getRandomPositiveInteger(MIN_COMMENTS, MAX_COMMENTS) }, createComment),
   };
