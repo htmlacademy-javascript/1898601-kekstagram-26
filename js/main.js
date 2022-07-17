4fd5351a6fce3d49e5aa6ec5fff283fb0623bf35
Anton Fomichev <avfomichev_2@edu.hse.ru>
2022-07-17 12:25:01 +0300

    fixed main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 import { MAX_STRING_LENGTH, PHOTOS_SIZE, createPhoto } from './data.js';
 import {
   getRandomPositiveInteger
-  , checkStringLength,
+  , checkStringLength, returnArray
 } from './util.js';
 
 getRandomPositiveInteger(1, 5);
 checkStringLength('Hello my name is...', MAX_STRING_LENGTH);
 const photos = Array.from({ length: PHOTOS_SIZE }, (elem, index) => createPhoto(index));
+returnArray(photos);
