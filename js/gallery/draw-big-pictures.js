cf92399105ea4c34c0d283331281e9ecc2707fa2
Anton Fomichev <avfomichev_2@edu.hse.ru>
2022-07-28 19:42:02 +0300

    Comments partially loading added.

--- js/gallery/draw-big-pictures.js
+++ js/gallery/draw-big-pictures.js
@@ -1,24 +1,11 @@
-import { appendItemsToList, isEscapeKey } from '../util.js';
+import { appendNElementsToList, isEscapeKey } from '../util.js';
 const bigPictureElement = document.querySelector('.big-picture');
 const commentsList = bigPictureElement.querySelector('.social__comments');
 const commentsListItem = commentsList.querySelector('.social__comment');
 const bigPictureCancel = bigPictureElement.querySelector('.big-picture__cancel');
-
-
-const closeBigPicture = function () {
-  document.body.classList.remove('modal-open');
-  bigPictureElement.classList.add('hidden');
-  bigPictureCancel.removeEventListener('click', closeBigPicture);
-  document.removeEventListener('keydown', onBigPictureEscKeyDown);
-};
-
-
-function onBigPictureEscKeyDown(evt) {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    closeBigPicture();
-  }
-}
+const commentsLoader = bigPictureElement.querySelector('.comments-loader');
+const commentInput = bigPictureElement.querySelector('.social__footer-text');
+const COMMENTS_ADD_STEP = 5;
 
 
 const makeCommentsListItem = function ({ avatar, name, message }) {
@@ -33,8 +20,6 @@ const makeCommentsListItem = function ({ avatar, name, message }) {
 
 const drawBigPicture = function ({ url, likes, comments, description }) {
   commentsList.innerHTML = '';
-  bigPictureElement.querySelector('.social__comment-count').classList.add('hidden');
-  bigPictureElement.querySelector('.comments-loader').classList.add('hidden');
 
   bigPictureCancel.addEventListener('click', closeBigPicture);
   document.addEventListener('keydown', onBigPictureEscKeyDown);
@@ -43,11 +28,50 @@ const drawBigPicture = function ({ url, likes, comments, description }) {
   bigPictureElement.querySelector('.likes-count').textContent = likes;
   bigPictureElement.querySelector('.comments-count').textContent = comments.length;
   bigPictureElement.querySelector('.social__caption').textContent = description;
+  bigPictureElement.querySelector('.comments-loader').classList.remove('hidden');
 
-  appendItemsToList(comments, commentsList, makeCommentsListItem);
+  const appendCommentsToList = appendNCommentsToList();
+  appendCommentsToList();
+  commentsLoader.addEventListener('click', appendCommentsToList);
 
   bigPictureElement.classList.remove('hidden');
   document.body.classList.add('modal-open');
+
+
+  function closeBigPicture() {
+    document.body.classList.remove('modal-open');
+    bigPictureElement.classList.add('hidden');
+    bigPictureCancel.removeEventListener('click', closeBigPicture);
+    document.removeEventListener('keydown', onBigPictureEscKeyDown);
+    commentsLoader.removeEventListener('click', appendCommentsToList);
+  }
+
+
+  function appendNCommentsToList() {
+    const addComments = appendNElementsToList(comments, commentsList, makeCommentsListItem, COMMENTS_ADD_STEP);
+    return function () {
+      let commentsAmount = commentsList.children.length;
+      if (commentsAmount < comments.length) {
+        addComments();
+        commentsAmount = commentsList.children.length;
+      }
+      if (commentsAmount === comments.length) {
+        commentsLoader.classList.add('hidden');
+        commentsLoader.removeEventListener('click', appendCommentsToList);
+      }
+      document.querySelector('.comments-count-now').textContent = commentsAmount;
+    };
+  }
+
+
+  function onBigPictureEscKeyDown(evt) {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      if (evt.target !== commentInput) {
+        closeBigPicture();
+      }
+    }
+  }
 };
 
 
